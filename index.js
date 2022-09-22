91380397d2dddbf75d7636e07fd2900ae379d340
Eduardo Lange <105828562+edugod@users.noreply.github.com>
2022-09-22 16:48:31 -0300

    updating

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
 const express = require("express");
-const app = express();
-// o const express é um require o npm
-// o const app é a função chamada
+// qq o express faz?
+const app = express(); //chamando o express como função
+// criei uma nova aplicação pra mim
 
-app.set('view engine', 'ejs')
-
-/* caso o programa procure o "/" através do get e não ache
-a gente vai criar uma forma de ele chegar no public */
-app.use(express.static('public'))
+app.set('view engine', 'ejs') //eu quero deixar o js limpo, chamar o html separadamente
+// com esse app.set o express vai procurar algo no meu diretório (pastas)
+// ele espera encontrar um diretório chamado "views"
 
 
+//caso ele bata no "/" do get e não ache nada o que fazemos?
+//enviamos ele para o public, como?
+app.use(express.static('public')) //aqui eu falo, olha se tu não achar o barra, vai pro diretorio "public"
 
 app.get("/", (request, response) => {
-	// console.log(new Date()); // to utilizando ejs agora
-	//response.send("<h1>Olá dutera</h1>"); (pq vamos utilizar o ejs)
-	response.render('home', {
-		date: new Date()
-	})
-});
-// se eu fizesse só isso não acontece nada
-// pra isso a gente precisa um listener
+	//se vc pegar '/' do website
+	//como funciona isso? quando o express recebe o request em barra via GET
+	// como eu quero develver a informação para o browser
+	// eu erspondo com uma mensagem
+   // console.log(new Date)
 
-app.listen(2800, (err) => {
+
+   // aqui é o EJS funcionando, ele vai chamar o home.ejs e depois eu posso mandar valores
+    response.render('home', { 
+    })  
+});
+app.get("/vaga", (request, response) => {
+
+    response.render('vaga', { 
+        
+    })  
+});
+
+// só que eu ainda preciso: "falar pra minha máquina quando ele pegar a / eu vou responder"
+// para fazer isso, usamos o listen
+app.listen(3000, (err) => {
+	//a função vai retornar um erro se não conseguir abrir a porta
 	if (err) {
-		console.log("não foi possível iniciar o servidor do jobify");
+		console.log("não foi possível iniciar o servidor");
 	} else {
-		console.log("servidor jobify rodando");
+		console.log("Servidor Jobify rodando...");
+        //posso abrir uma janela com "localhost:3000"
 	}
-});
-// o listen é um serviço.
-// os serviços ouvem numa porta na máquina
-// qnd eu dou um listen, eu to abrindo uma porta pra entrar
-// se usa portas altas, para fins de teste
-// na hora q for upar pro servidor precisa colocar 80 para http ou 443 para https
-// o err é para retornar um erro caso não consiga abrir a porta
-
+}); //portas < 1000 são portas "privilegiadas", 80 HTTP, 443 HTTPS (numero das portas)
